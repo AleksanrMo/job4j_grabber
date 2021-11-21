987c38f657a3d9929906fb81c390c800c56de875
AleksandrMo <sushanta@mail.ru>
2021-11-21 12:15:12 +0700

    quartz

---
+++ src/main/java/ru/job4j/quartz/ReadFIle.java
@@ -0,0 +1,20 @@
+package ru.job4j.quartz;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Path;
+
+public class ReadFIle {
+
+    public int time(String file) {
+        int rsl = 0;
+         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String[] st = reader.readLine().split("=");
+            rsl = Integer.parseInt(st[1]);
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
+         return rsl;
+    }
+}
