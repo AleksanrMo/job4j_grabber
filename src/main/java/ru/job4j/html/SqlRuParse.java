8496a6284c8477da3d681c1a9c740925434849c8
AleksandrMo <sushanta@mail.ru>
2021-11-23 22:10:58 +0700

    html

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,17 +4,22 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.util.LinkedHashSet;
+
 
 public class SqlRuParse {
 
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect(
-                "https://www.sql.ru/forum/1323240/sberbank-vedushhiy-inzhener-po-soprovozhdeniu-ot-120-000-rub").get();
-        Elements row = doc.select(".msgFooter");
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".altCol");
+        LinkedHashSet<String> list = new LinkedHashSet();
         for (Element td : row) {
             Element parent = td.parent();
-            String[] st = parent.child(0).text().split("\\[");
-            System.out.println(st[0]);
+            String st = parent.child(5).text();
+            list.add(st);
+        }
+        for(String dates: list) {
+            System.out.println(dates);
         }
     }
 }
