5d737f044f31889b364add4f0a1a9a7b067274bf
AleksandrMo <sushanta@mail.ru>
2021-11-24 21:03:17 +0700

    html

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -1,6 +1,10 @@
 package ru.job4j.html;
 
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import ru.job4j.quartz.GetDates;
 
 import java.util.ArrayList;
@@ -10,8 +14,15 @@ import java.util.List;
 public class SqlRuParse {
 
     public static void main(String[] args) throws Exception {
-        GetDates datesFromSql = new GetDates();
-        datesFromSql.dates(" https://www.sql.ru/forum/job-offers", ".altCol");
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td : row) {
+            Element el = td.parent();
+            Element href = td.child(0);
+            System.out.println(href.text());
+            System.out.println(href.attr("href"));
+            System.out.println(el.child(5).text().trim());
+        }
 
     }
 }
