d8f9ed5c8bc7289df1361ee211e60c2c4dc43139
AleksandrMo <sushanta@mail.ru>
2021-11-25 14:21:09 +0700

    html

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -11,14 +11,17 @@ public class SqlRuParse {
 
     public static void main(String[] args) throws Exception {
         SqlRuDayTimeParser parser = new SqlRuDayTimeParser();
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element el = td.parent();
-            Element href = td.child(0);
-            System.out.println(href.text());
-            System.out.println(href.attr("href"));
-            System.out.println(parser.parse(el.child(5).text().trim()));
+        for (int i = 1; i <= 5; i++) {
+            String string = String.format("https://www.sql.ru/forum/job-offers/%d", i);
+            Document doc = Jsoup.connect(string).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element el = td.parent();
+                Element href = td.child(0);
+                System.out.println(href.text());
+                System.out.println(href.attr("href"));
+                System.out.println(parser.parse(el.child(5).text().trim()));
+            }
         }
     }
 }
