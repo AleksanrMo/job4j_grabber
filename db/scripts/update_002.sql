d5e1df1119015b36c25686dc4e39d4ca28046bf6
AleksandrMo <sushanta@mail.ru>
2021-11-26 20:03:14 +0700

    html

--- db/scripts/update_002.sql
+++ db/scripts/update_002.sql
@@ -2,6 +2,6 @@ create table if not exists posts(
     id serial primary key,
     name varchar(255),
     description text,
-    link varchr(255) UNIQUE,
+    link varchar(255) UNIQUE,
     created timestamp
 );
