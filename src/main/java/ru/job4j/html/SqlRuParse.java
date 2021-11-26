1dd34f192deca0c4b3fc97c0fe47a32297cbbaaf
AleksandrMo <sushanta@mail.ru>
2021-11-26 16:39:08 +0700

    html

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,40 +7,33 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import ru.job4j.utils.Post;
 import ru.job4j.utils.SqlRuDayTimeParser;
-
+import ru.job4j.utils.DayTimeParser;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
 
-    public static void main(String[] args) throws Exception {
-        for (int i = 1; i <= 5; i++) {
-            String string = String.format("https://www.sql.ru/forum/job-offers/%d", i);
-            returnPosts(string).forEach(System.out::println);
-       }
+    private final DayTimeParser dayTimeParser;
+
+    public SqlRuParse(DayTimeParser dayTimeParser) {
+        this.dayTimeParser = dayTimeParser;
     }
 
-    public static String getMessage(String url) throws IOException {
-        Document doc = Jsoup.connect(
-                url).get();
-        Elements elements = doc.select(".msgBody");
-        return elements.get(1).text();
-    }
-
-    public static List<Post> returnPosts(String url) throws IOException {
+    @Override
+    public  List<Post> list(String url) throws IOException {
         List<Post> list = new ArrayList<>();
         Document doc = Jsoup.connect(url).get();
         Elements row = doc.select(".postslisttopic");
         for (Element td : row) {
-            list.add(getPost(td.child(0).attr("href")));
+            list.add(detail(td.child(0).attr("href")));
         }
         return list;
     }
 
-        public static Post getPost(String link) throws IOException {
-        SqlRuDayTimeParser parser = new SqlRuDayTimeParser();
+    @Override
+        public  Post detail(String link) throws IOException {
         Document doc = Jsoup.connect(link).get();
             Elements elements = doc.select(".msgTable");
             Element e = elements.get(0);
@@ -48,8 +41,16 @@ public class SqlRuParse {
             String description = e.child(0).child(1).text();
             String[]  date = e.child(0).child(2).child(0).text().split("\\[");
             int id = Integer.parseInt(e.child(0).child(2).child(0).child(0).text());
-            LocalDateTime  localDateTime = parser.parse(date[0].trim());
+            LocalDateTime  localDateTime = dayTimeParser.parse(date[0].trim());
             return new Post(id, header, link, description, localDateTime);
 
         }
+
+    public static void main(String[] args) throws Exception {
+        SqlRuParse salParse = new SqlRuParse(new SqlRuDayTimeParser());
+        for (int i = 1; i <= 5; i++) {
+            String string = String.format("https://www.sql.ru/forum/job-offers/%d", i);
+            salParse.list(string).forEach(System.out::println);
+        }
+    }
 }
